689f3916741ae14398e3e7ad2e274570d6317752
Jonas Munsin <jm@berggatan.(none)>
2009-01-08 12:55:34 +0200

    added .JPG support

--- myapp.rb
+++ myapp.rb
@@ -40,15 +40,24 @@ def get_page(dir, sub=false)
   d = get_dirs(dir, sub) + BlueCloth.new("---\n").to_html + get_files(dir, sub)
 end
 
+def get_jpg(pvdir, params, suffix)
+  imgs = Magick::Image.read(pvdir + "/" + params["splat"].join("/") + suffix)
+  img = imgs.first
+  img.resize!(0.1)
+  content_type 'image/jpg'
+  img.to_blob { fileformat="JPEG" }
+end
+
 get '/' do
   get_page(pvdir)
 end
 
 get '/*.jpg' do
-  img = Magick::Image.read(pvdir + "/" + params["splat"].join("/") + ".jpg").first
-  img.resize!(0.1)
-  content_type 'image/jpg'
-  img.to_blob { fileformat="JPEG" }
+  get_jpg(pvdir, params, ".jpg")
+end
+
+get '/*.JPG' do
+  get_jpg(pvdir, params, ".JPG")
 end
 
 get '/*' do
